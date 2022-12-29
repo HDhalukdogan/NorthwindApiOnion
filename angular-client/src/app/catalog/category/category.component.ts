c84c29c97be961760700149037e171892540d4a4
haluk.dogan@dmo.gov.tr <haluk.dogan@dmo.gov.tr>
2022-12-29 14:47:40 +0300

    checkbox

--- angular-client/src/app/catalog/category/category.component.ts
+++ angular-client/src/app/catalog/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { CatalogService } from '../catalog.service';
 
 @Component({
@@ -8,36 +9,72 @@ import { CatalogService } from '../catalog.service';
 })
 export class CategoryComponent implements OnInit {
   categories: any = [];
-  selectedOption;
-  selectedCategory="";
-  constructor(private catalogService: CatalogService) { }
+  selectedCategory: any = {};
+  form: FormGroup;
 
+
+  constructor(private catalogService: CatalogService, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      checkArray: this.fb.array([])
+    })
+  }
 
   ngOnInit(): void {
     this.getCategories();
   }
 
-
-  getCategory() {
-    console.log('this.selectedOption', typeof this.selectedOption)
-    this.selectedCategory = this.categories.find(s => s.categoryId == this.selectedOption)
+  getProducts(id) {
+    this.selectedCategory = this.categories.find(s => s.categoryId == id)
     console.log('selectedCategory', this.selectedCategory)
   }
-
-  getProducts(id){
-    this.selectedCategory = this.categories.find(s => s.categoryId == id)
-    console.log('id', id)
-  }
-
 
   getCategories() {
     this.catalogService.getCategories().subscribe({
       next: (res: any) => {
         this.categories = res
+        this.getProducts(1)
         console.log('this.categories', this.categories)
       },
       error: (err) => console.log('err', err)
     })
   }
 
+  onCheckboxChange(e) {
+    const checkArray: FormArray = this.form.get('checkArray') as FormArray;
+    if (e.target.checked) {
+      checkArray.push(new FormControl(e.target.value));
+    } else {
+      // let i: number = 0;
+      // checkArray.controls.forEach((item: FormControl) => {
+      //   if (item.value == e.target.value) {
+      //     checkArray.removeAt(i);
+      //     return;
+      //   }
+      //   i++;
+      // });
+      let index = checkArray.controls.findIndex(s => s.value == e.target.value);
+      checkArray.removeAt(index)
+    }
+  }
+
+  onSelectAllChange(e) {
+    console.log('e', e)
+    const checkArray: FormArray = this.form.get('checkArray') as FormArray;
+    if (e) {
+      this.selectedCategory.products.forEach((product: any) => checkArray.push(new FormControl(product.productId)))
+    } else {
+      // let i: number = 0;
+      // checkArray.controls.forEach((item: FormControl) => {
+      //     checkArray.removeAt(i);
+      //   i++;
+      // });
+      checkArray.clear()
+    }
+  }
+
+  onSubmit() {
+    console.log('this.form.value', this.form.value)
+    this.form.reset();
+    this.onSelectAllChange(false)
+  }
 }
